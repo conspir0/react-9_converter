4136204b073baeff23eda5c6bbccb655cd99f88a
conspir0 <michalczyk.mat3usz@gmail.com>
2020-09-09 23:09:12 +0200

    Create func.comp. ValueToConvert, ProductSelect, ProductItem.

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 class App extends React.Component {
   state = {
     inputValue: '',
-    ratio: {
-      euro: 4.60,
-      dollar: 3.90
-    }
+    selectedItem: '',
+    currency: [
+      { id: 1, title: 'dollar', ratio: 4.60, sign: '$' },
+      { id: 2, title: 'euro', ratio: 3.90, sign: '€' },
+      { id: 3, title: 'pound', ratio: 4.90, sign: '£' },
+    ],
+    products: [
+      { id: 0, title: '', price: 0 },
+      { id: 1, title: 'gas', price: 2.30 },
+      { id: 2, title: 'oil', price: 4.10 },
+      { id: 3, title: 'petrol', price: 4.50 },
+    ]
   }
 
   inputHandler = (e) => {
@@ -15,38 +23,47 @@ class App extends React.Component {
     }
   }
 
-  converterHandler = (value, ratio) => {
-    return (value / ratio).toFixed(2);
+  selectHandler = (e) => {
+    this.setState({
+      selectedItem: e.target.value
+    })
+  }
+
+  converterHandler = (qty, ratio, list, product) => {
+    const price = list.filter(elem => elem.title === product);
+
+    return (qty * price[0].price / ratio).toFixed(2);
   }
 
   render() {
-    const { inputValue, ratio } = this.state;
+    const { inputValue, currency, products, selectedItem } = this.state;
+    const resultBoxList = currency.map(elem => {
+      return (
+        <ResultBox
+          key={elem.id}
+          title={elem.title}
+          sign={elem.sign}
+          convertedValue={this.converterHandler(inputValue, elem.ratio, products, selectedItem)}
+        />
+      )
+    })
 
     return (
       <React.Fragment>
         <div className="container">
           <h1 className="title-container">
-            Write value in PLN
+            Check price in other currency
           </h1>
-          <input
-            type="number"
-            placeholder="write value"
-            className="input-text"
+          <ProductSelect
+            products={products}
+            selectEvent={this.selectHandler}
+          />
+          <ValueToConvert
             value={inputValue}
-            min="0"
-            onChange={this.inputHandler}
+            inputEvent={this.inputHandler}
           />
           <div className="result-container">
-            <ResultBox
-              title="dollar"
-              sign="$"
-              convertedValue={this.converterHandler(inputValue, ratio.dollar)}
-            />
-            <ResultBox
-              title="euro"
-              sign="€"
-              convertedValue={this.converterHandler(inputValue, ratio.euro)}
-            />
+            {resultBoxList}
           </div>
         </div>
       </React.Fragment>
@@ -62,4 +79,46 @@ const ResultBox = ({ title, sign, convertedValue }) => {
   )
 }
 
+const ValueToConvert = ({ value, inputEvent }) => {
+  return (
+    <input
+      type="number"
+      placeholder="write value"
+      className="input-text"
+      value={value}
+      min="0"
+      onChange={inputEvent}
+    />
+  )
+}
+
+const ProductSelect = ({ products, selectEvent }) => {
+  const elements = products.map(product => {
+    return (
+      <ProductItem
+        key={product.id}
+        title={product.title}
+      />
+    )
+  })
+
+  return (
+    <select
+      className="input-text"
+      onChange={selectEvent}
+    >
+      {elements}
+    </select>
+  )
+}
+
+const ProductItem = ({ title }) => {
+  return (
+    <option
+      value={title}
+    >
+      {title}
+    </option>
+  )
+}
 ReactDOM.render(<App />, document.getElementById('root'));

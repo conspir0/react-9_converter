3f4868a26897509cd9f972cdb59d4367db7f80d8
conspir0 <michalczyk.mat3usz@gmail.com>
2020-09-09 22:28:23 +0200

    Create class App, func.comp. ResultBox

--- App.js
+++ App.js
@@ -1,17 +1,65 @@
 class App extends React.Component {
-  state = {}
+  state = {
+    inputValue: '',
+    ratio: {
+      euro: 4.60,
+      dollar: 3.90
+    }
+  }
+
+  inputHandler = (e) => {
+    if (e.target.value >= 0) {
+      this.setState({
+        inputValue: e.target.value,
+      })
+    }
+  }
+
+  converterHandler = (value, ratio) => {
+    return (value / ratio).toFixed(2);
+  }
 
   render() {
-    const { qty, amount, list } = this.state;
+    const { inputValue, ratio } = this.state;
 
     return (
       <React.Fragment>
         <div className="container">
-          Hello World!
+          <h1 className="title-container">
+            Write value in PLN
+          </h1>
+          <input
+            type="number"
+            placeholder="write value"
+            className="input-text"
+            value={inputValue}
+            min="0"
+            onChange={this.inputHandler}
+          />
+          <div className="result-container">
+            <ResultBox
+              title="dollar"
+              sign="$"
+              convertedValue={this.converterHandler(inputValue, ratio.dollar)}
+            />
+            <ResultBox
+              title="euro"
+              sign="€"
+              convertedValue={this.converterHandler(inputValue, ratio.euro)}
+            />
+          </div>
         </div>
       </React.Fragment>
     )
   }
+}
+
+const ResultBox = ({ title, sign, convertedValue }) => {
+  return (
+    <div className="result-box">
+      {title}: {convertedValue} {sign}
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
